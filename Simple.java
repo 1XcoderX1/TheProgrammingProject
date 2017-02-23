3d727612f7fed00bac4bd3e17831113e9f5145bf
Archit <architk1105@gmail.com>
2017-02-23 18:51:01 +0000

    added Part1 files

---
+++ Simple.java
@@ -0,0 +1,16 @@
+package everythingImp;
+
+public class Simple {
+	
+	public Integer sum(int x, int y){
+		
+		return x+y;
+		
+		
+	}
+	
+	
+	
+	
+	
+}
---
+++ SimpleTest.java
@@ -0,0 +1,15 @@
+package everythingImp;
+
+public class SimpleTest {
+
+	public static void main(String[] args) {
+		
+		
+		Simple andreisSimple = new Simple();
+		
+		System.out.print(andreisSimple.sum(-1,20));
+		
+		
+	}
+
+}
