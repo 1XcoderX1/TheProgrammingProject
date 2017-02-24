4df5410e2e36b75e748d98d2a93a80f26e780efa
acm634 <acm634@cs.bham.ac.uk>
2017-02-24 12:53:05 +0100

    I made the classes and the README.md file

---
+++ Homework/Calculator.java
@@ -0,0 +1,26 @@
+package acm634;
+
+public class Calculator {
+	
+	public double sum(double x, double y){
+		
+		return x + y;
+	}
+	
+	public double substraction(double z, double w){
+		
+		return z - w;
+	}
+	
+	public double multiplication(double q, double r){
+		
+		return q * r;
+		
+	}
+	
+	public double division(double a, double b){
+		
+		return a / b;
+	}
+
+}
---
+++ Homework/CalculatorTest.java
@@ -0,0 +1,16 @@
+package acm634;
+
+public class CalculatorTest {
+
+	public static void main(String[] args) {
+		
+		Calculator calculator = new Calculator();
+		
+		System.out.println(calculator.sum(x, y));
+		System.out.println(calculator.substraction(z, w));
+		System.out.println(calculator.multiplication(q, r));
+		System.out.println(calculator.division(a, b));
+
+	}
+
+}
